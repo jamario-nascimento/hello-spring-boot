1adb1b56a0436ea1d9b782e9639868753beb6467
Jamário Nascimento <jamariobatista@gmail.com>
2023-11-04 15:24:21 -0300

    application properties

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,11 @@
 package org.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 /**
  * Hello world!
  *
@@ -14,9 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class App
 {
-    @Autowired
-    @Qualifier("applicationName")
+
+//    @Autowired
+//    @Qualifier("applicationName")
+//    private String applicationName;
+
+    @Value("${application.name}")
     private String applicationName;
+
     @GetMapping("/hello")
     public String helloWorld(){
         return applicationName;
