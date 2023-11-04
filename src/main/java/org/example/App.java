3dd80a3e9ef7bd6bfcd43a44124274cd08cd1adc
Jamário Nascimento <jamariobatista@gmail.com>
2023-11-04 13:26:27 -0300

    hello World Spring

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,25 @@
+package org.example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@RestController
+public class App 
+{
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+}
