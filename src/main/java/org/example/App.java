cb852267faa9934b317c9622426985c0b97fc7ed
Jamário Nascimento <jamariobatista@gmail.com>
2023-11-04 16:26:26 -0300

    profiles configurarion

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,8 +2,10 @@ package org.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 /**
@@ -22,6 +24,16 @@ class App
     @Value("${application.name}")
     private String applicationName;
 
+    @Autowired
+    @Qualifier("cachorro")
+    private Animal animal;
+
+    @Bean
+    public CommandLineRunner execute(){
+        return args -> {
+            this.animal.fazerBarrulho();
+        };
+    }
     @GetMapping("/hello")
     public String helloWorld(){
         return applicationName;
--- src/main/java/org/example/MyConfiguration.java
+++ src/main/java/org/example/MyConfiguration.java
@@ -1,12 +1,15 @@
 package org.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Development
 public class MyConfiguration {
-    @Bean(name = "applicationName")
-    public String applicationName(){
-        return "Sistemas de Vendas";
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("Executing in Development");
+        };
     }
 }
